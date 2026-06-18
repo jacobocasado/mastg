1e88cf4c0af5d02697e55626c8292267b044a2a2
Jacobo Casado <casadojacobo@gmail.com>
2026-06-18 17:43:40 +0200

    Remove duplicate exported activity content

--- demos/android/MASVS-CODE/MASTG-DEMO-0137/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# SUMMARY: This script uses semgrep to find all exported activities in the manifest.
-NO_COLOR=true semgrep --config rule.yaml ../MASTG-DEMO-0136/AndroidManifest_reversed.xml --text > output.txt
